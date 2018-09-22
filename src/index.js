9a61e8741ef10451826bd58ce93d0a9977bc0b9c
enitharmon <hecateenitharmon@gmail.com>
2018-09-22 11:42:54 +0300

    additional-5/fix: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var currentRegexp = "";
+    if (/\d/g.test(bracketsConfig)) {
+        for (var a = 0; a < bracketsConfig.length; a++) {
+            currentRegexp = currentRegexp + "|" + bracketsConfig[a][0] + bracketsConfig[a][1];
+        }
+    } else {
+        for (var b = 0; b < bracketsConfig.length; b++) {
+            currentRegexp = currentRegexp + "|" + "\\" + bracketsConfig[b][0] + "\\" + bracketsConfig[b][1];
+        }
+    }
+    currentRegexp = currentRegexp.substring(1);
+
+    var regexpObject;
+    regexpObject = new RegExp(currentRegexp);
+
+    var newStr = str;
+    do {
+        newStr = newStr.replace(regexpObject, '');
+    } while (newStr.match(regexpObject));
+    if (newStr.length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
